d1bdcdd425b679215703b1196685120cacbf6507
maxim_stoma <maksim.stoma@mail.ru>
2022-04-27 08:26:04 +0300

    start solving the second task

---
+++ task001_max_number_mod/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter first number ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
