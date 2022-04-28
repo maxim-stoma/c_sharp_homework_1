17f646f90d17d32ffcf54d340e16b52d6c4953b6
maxim_stoma <maksim.stoma@mail.ru>
2022-04-28 21:46:19 +0300

    fourth task solved

--- task003_even_range/Program.cs
+++ task003_even_range/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача. Напишите программу, которая на вход принимает число (N),
 // а на выходе показывает все чётные числа от 1 до (N)
 
+Console.WriteLine("Enter the number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= num; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(i);
+
+        if(i != num)
+        {
+            Console.Write(",");
+        }   
+    }    
+}
