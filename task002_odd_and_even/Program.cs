7f8b3809f0f6ba96cbb660a5e6644fb39a540ebb
maxim_stoma <maksim.stoma@mail.ru>
2022-04-28 08:19:19 +0300

    third task solved

---
+++ task002_odd_and_even/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter a number ");
+int num = Convert.ToInt32(Console.Read());
+int par = num % 2;
+
+if(par == 0) Console.WriteLine("The number is even");
+if(par == 1) Console.WriteLine("The number is odd");
