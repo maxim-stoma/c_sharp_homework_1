ca0c25ae176adbce0c4de3484f0bbd22692bbc23
maxim_stoma <maksim.stoma@mail.ru>
2022-04-28 08:01:05 +0300

    second task solved

--- task001_max_number_mod/Program.cs
+++ task001_max_number_mod/Program.cs
@@ -4,4 +4,12 @@ Console.WriteLine("Enter second number ");
 int secondNum = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter third number ");
 int thirdNum = Convert.ToInt32(Console.ReadLine());
+int max = firstNum;
 
+
+if (firstNum > max) max = firstNum;
+if (secondNum > max) max = secondNum;
+if (thirdNum > max) max = thirdNum;
+
+Console.Write("The largest number of three is ");
+Console.WriteLine(max);
