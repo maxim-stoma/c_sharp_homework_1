c121dbd64b49c485472dfea061e3a697b1019a24
maxim_stoma <maksim.stoma@mail.ru>
2022-04-29 11:28:44 +0300

    alternative solvation (task 2.1) realized keyboard array input

---
+++ task001.1_max_number_mod/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача. Напишите программу, которая принимает на вход три числа,
+// и выдаёт максимальное из этих чисел 
+
+Console.Write("Enter the number of array elements:\t");
+
+int n = int.Parse(Console.ReadLine());
+
+int[] array = new int[n];
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Enter an array element number {i}:\t ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Displaying array elements: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+    if (i <= array.Length - 2)
+    {
+        Console.Write(", "); 
+    }
+}
+Console.ReadLine();
